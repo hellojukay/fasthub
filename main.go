a4b56db581f324f952c737e0749b596e41f38a2b
hellojukay <hellojukay@163.com>
2020-08-03 15:36:30 +0800

    fix http checl bug

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
+// https://github.com/hellojukay/fasthub
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net"
@@ -18,7 +21,21 @@ type Meta struct {
 	Git []string `json:"git"`
 }
 
-func githubIPS(api string) ([]string, error) {
+const (
+	// HTTP http connection  ip
+	HTTP = "HTTP"
+	// SSH ssh conection ip
+	SSH = "SSH"
+)
+
+// Address git ip address
+type Address struct {
+	IP string
+	// SSH or http
+	TYPE string
+}
+
+func githubIPS(api string) ([]Address, error) {
 	var client = http.DefaultClient
 	res, err := client.Get(api)
 	if err != nil {
@@ -28,41 +45,83 @@ func githubIPS(api string) ([]string, error) {
 	var meta Meta
 	body, err := ioutil.ReadAll(res.Body)
 	json.Unmarshal(body, &meta)
-	var ips = append(meta.Git, meta.Web...)
-	var result []string
-	for _, ip := range ips {
-		result = append(result, strings.Split(ip, "/")[0])
+	var result []Address
+	for _, ip := range meta.Web {
+		var arr = strings.Split(ip, "/")
+		if arr[1] != "32" {
+			continue
+		}
+		result = append(result, Address{
+			IP:   arr[0],
+			TYPE: HTTP,
+		})
+	}
+	for _, ip := range meta.Git {
+		var arr = strings.Split(ip, "/")
+		if arr[1] != "32" {
+			continue
+		}
+		result = append(result, Address{
+			IP:   arr[0],
+			TYPE: SSH,
+		})
 	}
 	return result, nil
 }
 
 // check ssh connection time
-func checkSSH(ip string) (int, error) {
-	var startTime = time.Now().Nanosecond()
-	_, err := net.DialTimeout("tcp", fmt.Sprintf("%s:22", ip), time.Duration(10)*time.Second)
+func checkSSH(ip string) (float64, error) {
+	var startTime = time.Now()
+	con, err := net.DialTimeout("tcp", fmt.Sprintf("%s:22", ip), time.Duration(10)*time.Second)
 	if err != nil {
-		return 0, err
+		return 0, errors.New("TIMEOUT")
 	}
-	var endTime = time.Now().Nanosecond()
-	return endTime - startTime, nil
+	con.SetDeadline(time.Now().Add(time.Second * 5))
+	var buf = make([]byte, 1)
+	con.Read(buf)
+	var endTime = time.Now()
+	var duration = endTime.Sub(startTime)
+	return duration.Seconds(), nil
+}
+
+// check http connection time
+func checkHTTP(ip string) (float64, error) {
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	var startTime = time.Now()
+	res, err := http.Get(fmt.Sprintf("http://%s/hellojukay", ip))
+	if err != nil {
+		return 0, errors.New("TIMEOUT")
+	}
+	defer res.Body.Close()
+	_, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		return 0, errors.New("TIMEOUT")
+	}
+	return time.Now().Sub(startTime).Seconds(), nil
 }
 
 //https://api.github.com/meta
 func main() {
 	var waitGroup sync.WaitGroup
-	ips, err := githubIPS("https://api.github.com/meta")
+	addresses, err := githubIPS("https://api.github.com/meta")
 	if err != nil {
 		fmt.Printf("request github error %s\n", err)
 		os.Exit(1)
 	}
-	for _, ip := range ips {
+	for _, ip := range addresses {
 		waitGroup.Add(1)
-		go func(ip string) {
-			t, err := checkSSH(ip)
-			if err != nil {
-				fmt.Printf("%-20s %10s", ip, err)
+		go func(address Address) {
+			var t float64
+			var err error
+			if address.TYPE == SSH {
+				t, err = checkSSH(address.IP)
 			} else {
-				fmt.Printf("%-20s %10d", ip, t)
+				t, err = checkHTTP(address.IP)
+			}
+			if err != nil {
+				fmt.Printf("%-20s%6s%8s\n", address.IP, err, address.TYPE)
+			} else {
+				fmt.Printf("%-20s%6s%8s\n", address.IP, fmt.Sprintf("%2.2fs", t), address.TYPE)
 			}
 		}(ip)
 	}
